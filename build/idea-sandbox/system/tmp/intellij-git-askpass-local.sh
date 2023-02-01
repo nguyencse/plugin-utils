41b6d918c1efd6d10ccd8c7827664fb147b88a2d
Nguyen Y Nguyen <nguyencse.ninhhoa@gmail.com>
2023-02-01 23:04:17 +0700

    Delete build directory

--- build/idea-sandbox/system/tmp/intellij-git-askpass-local.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-"/home/nguyenny/.gradle/caches/modules-2/files-2.1/com.jetbrains/jbre/jbr-11_0_10-linux-x64-b1145.96/jbr/bin/java" -cp "/opt/android-studio/plugins/git4idea/lib/git4idea-rt.jar:/opt/android-studio/lib/xmlrpc-2.0.1.jar:/opt/android-studio/lib/commons-codec-1.14.jar" org.jetbrains.git4idea.http.GitAskPassApp "$@"
